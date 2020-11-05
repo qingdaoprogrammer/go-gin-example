42fed47bb679879fc7639c8643a8c2bcb920b82f
Administrator <Administrator@PC-20170516KCVK>
2020-11-05 15:17:06 +0800

    新增日志功能

--- pkg/logging/file.go
+++ pkg/logging/file.go
@@ -1,4 +1,4 @@
-package loggin
+package logging
 
 import (
 	"fmt"
@@ -11,13 +11,15 @@ var (
 	LogSavePath = "runtime/logs/"
 	LogSaveName = "log"
 	LogFileExt  = "log"
-	TimeFormat  = "20060102"
+	TimeFormat  = "2006010201"
 )
 
+//获取日志保存路径
 func getLogFilePath() string {
 	return fmt.Sprintf("%s", LogSavePath)
 }
 
+//获取日志完成路径
 func getLogFileFullPath() string {
 	prefixPath := getLogFilePath()
 	suffixPath := fmt.Sprintf("%s%s.%s", LogSaveName, time.Now().Format(TimeFormat), LogFileExt)
@@ -25,15 +27,17 @@ func getLogFileFullPath() string {
 	return fmt.Sprintf("%s%s", prefixPath, suffixPath)
 }
 
+//打开日志文件
 func openLogFile(filePath string) *os.File {
 	_, err := os.Stat(filePath)
 	switch {
-	case os.IsNotExist(err):
+	case os.IsNotExist(err): //文件目录不存在
 		mkDir()
-	case os.IsPermission(err):
+	case os.IsPermission(err): //没有权限
 		log.Fatalf("Permission :%v", err)
 	}
 
+	//打开文件
 	handle, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("Fail to OpenFile :%v", err)
@@ -42,6 +46,7 @@ func openLogFile(filePath string) *os.File {
 	return handle
 }
 
+//创建目录
 func mkDir() {
 	dir, _ := os.Getwd()
 	err := os.MkdirAll(dir+"/"+getLogFilePath(), os.ModePerm)
--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -1,7 +1,7 @@
 package v1
 
 import (
-	"log"
+	"github.com/han/go-gin-example/pkg/logging"
 	"net/http"
 
 	"github.com/astaxie/beego/validation"
@@ -32,7 +32,7 @@ func GetArticle(c *gin.Context) {
 		}
 	} else {
 		for _, err := range valid.Errors {
-			log.Printf("err.key: %s, err.message: %s", err.Key, err.Message)
+			logging.Info("err.key: %s, err.message: %s", err.Key, err.Message)
 		}
 	}
 
@@ -73,7 +73,7 @@ func GetArticles(c *gin.Context) {
 		data["total"] = models.GetArticleTotal(maps)
 	} else {
 		for _, err := range valid.Errors {
-			log.Printf("err.key: %s, err.message: %s", err.Key, err.Message)
+			logging.Info("err.key: %s, err.message: %s", err.Key, err.Message)
 		}
 	}
 
@@ -119,7 +119,7 @@ func AddArticle(c *gin.Context) {
 		}
 	} else {
 		for _, err := range valid.Errors {
-			log.Printf("err.key: %s, err.message: %s", err.Key, err.Message)
+			logging.Info("err.key: %s, err.message: %s", err.Key, err.Message)
 		}
 	}
 
@@ -184,7 +184,7 @@ func EditArticle(c *gin.Context) {
 		}
 	} else {
 		for _, err := range valid.Errors {
-			log.Printf("err.key: %s, err.message: %s", err.Key, err.Message)
+			logging.Info("err.key: %s, err.message: %s", err.Key, err.Message)
 		}
 	}
 
@@ -212,7 +212,7 @@ func DeleteArticle(c *gin.Context) {
 		}
 	} else {
 		for _, err := range valid.Errors {
-			log.Printf("err.key: %s, err.message: %s", err.Key, err.Message)
+			logging.Info("err.key: %s, err.message: %s", err.Key, err.Message)
 		}
 	}
 
